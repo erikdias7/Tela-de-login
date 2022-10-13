16e1627d6a0a5f6f3bb263d039259074ba0a0c3d
erikdias7 <103780028+erikdias7@users.noreply.github.com>
2022-10-13 09:28:01 -0300

    .

---
+++ Projeto.py
@@ -0,0 +1,27 @@
+from PySimpleGUI import PySimpleGUI as sg
+from main import *
+from lugares import *
+#layout
+def interface():
+    sg.theme('DarkBlue7')
+    x = input()
+    y = input()
+    layout = [
+        [sg.Text('E-mail'),sg.Input(key=x,size=(20,1))],
+        [sg.Text('Senha'),sg.Input(key=y,password_char="*",size=(20,1))],
+        [sg.Checkbox('Salvar o Login?')],
+        [sg.Button('Entrar')]
+    ]
+    #janela
+    janela = sg.Window('Tela de Login', layout)
+    #ler os eventos 
+    while True:
+        eventos, valores = janela.read()
+        if eventos == sg.WINDOW_CLOSED:
+            break
+        if eventos == 'Entrar':
+            escolhe_reino()
+
+if(__name__ == "__main__"):        
+    interface()
+
